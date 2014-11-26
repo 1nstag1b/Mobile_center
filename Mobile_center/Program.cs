dca4a4eab0518d9a6b62088b6942b4cf0e679932
1nstag1b <tr.instagib@gmail.com>
2014-11-26 22:12:27 +0400

    MyFirstCommit

---
+++ Mobile_center/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MobileCenter
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+
+        public static mainForm mainForm;
+        public static authorizationForm authorizationForm;
+        public static productsForm productsForm;
+        public static servicesForm servicesForm;
+        public static customersForm customersForm;
+        public static staffForm staffForm;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            mainForm = new MobileCenter.mainForm();
+            authorizationForm = new MobileCenter.authorizationForm();
+            productsForm = new MobileCenter.productsForm();
+            servicesForm = new MobileCenter.servicesForm();
+            customersForm = new MobileCenter.customersForm();
+            staffForm = new MobileCenter.staffForm();
+            Application.Run(authorizationForm);
+        }
+    }
+}
---
+++ Mobile_center/mainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MobileCenter
+{
+    public partial class mainForm : Form
+    {
+        public void showChildForm(int n)
+        {
+            if (ActiveMdiChild != null)
+                ActiveMdiChild.Hide();
+
+            switch (n)
+            {
+                case 1:
+                    Program.productsForm.Show();
+                    break;
+                case 2:
+                    Program.servicesForm.Show();
+                    break;
+                case 3:
+                    Program.customersForm.Show();
+                    break;
+                case 4:
+                    Program.staffForm.Show();
+                    break;
+            }
+        }
+
+        public mainForm()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+
+        private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void productsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showChildForm(1);
+        }
+
+        private void servicesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showChildForm(2);
+        }
+
+        private void customersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showChildForm(3);
+        }
+
+        private void staffToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            showChildForm(4);
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ActiveMdiChild != null)
+                ActiveMdiChild.Hide();
+            Hide();
+            Program.authorizationForm.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }    
+    }
+}
